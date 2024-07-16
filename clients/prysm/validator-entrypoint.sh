6f547d3ba8780666243fa370ff779d0dcf4f4faa
Francis Li <huiyi.li@outlook.com>
2024-07-16 02:21:29 +0000

    Finish prysm validator config

--- clients/prysm/validator-entrypoint.sh
+++ clients/prysm/validator-entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 set -eu
 
+VALIDATOR_WALLET_DIR="${VALIDATOR_WALLET_DIR:-/data/wallet}"
+VALIDATOR_DATA_DIR="${VALIDATOR_DATA_DIR:-/data/validator}"
+VALIDATOR_GRAFFITI="${VALIDATOR_GRAFFITI:-0x00101010}"
+VALIDATOR_RPC_GATEWAY_PROVIDER="$VALIDATOR_BEACON_NODE_DNS:$BEACON_NODE_GRPC_PORT"
+VALIDATOR_RPC_PROVIDER="$VALIDATOR_BEACON_NODE_DNS:$BEACON_NODE_RPC_PORT"
+
+ADDITIONAL_ARGS=""
+
+./validator \
+    --accept-terms-of-use \
+    --$NETWORK \
+    --wallet-dir=$VALIDATOR_WALLET_DIR \
+    --wallet-password-file=$VALIDATOR_WALLET_PASSWORD_FILE \
+    --datadir=$VALIDATOR_DATA_DIR \
+    --graffiti=$VALIDATOR_GRAFFITI \
+    --suggested-fee-recipient=$VALIDATOR_SUGGESTED_FEE_RECIPIENT \
+    --beacon-rest-api-provider=$VALIDATOR_RPC_GATEWAY_PROVIDER \
+    --beacon-rpc-provider=$VALIDATOR_RPC_PROVIDER \
+    $ADDITIONAL_ARGS # intentionally unquoted
