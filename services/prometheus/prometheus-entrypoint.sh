78bb02c9654418d1b1b234fbaaf6f53e17a0308b
Francis Li <huiyi.li@outlook.com>
2024-07-18 23:02:00 +0000

    Finish prometheus configs

--- services/prometheus/prometheus-entrypoint.sh
+++ services/prometheus/prometheus-entrypoint.sh
@@ -2,9 +2,11 @@
 set -eu
 
 PROMETHEUS_PORT="${PROMETHEUS_PORT:-9090}"
+PROMETHEUS_LOG_LEVEL="${PROMETHEUS_LOG_LEVEL:-info}"
 TEMPLATE_CONFIG_FILE="${TEMPLATE_CONFIG_FILE:-./prometheus-config-template.yml}"
 envsubst < $TEMPLATE_CONFIG_FILE > "prometheus.yml"
 
 exec ./prometheus \
-    --log.level=debug \
+    --log.level="$PROMETHEUS_LOG_LEVEL" \
+    --storage.tsdb.path=/data \
     --web.listen-address="0.0.0.0:$PROMETHEUS_PORT"
