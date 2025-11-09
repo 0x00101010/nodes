1d1d3102d0671c8b660f7ca166c929a4f41d7521
Francis Li <francis.li@uniswap.org>
2025-11-08 21:51:55 -0800

    minor bug fix

--- blockchains/uni/mainnet/up.sh
+++ blockchains/uni/mainnet/up.sh
@@ -48,6 +48,6 @@ fi
 
 # Launch with the selected profile
 echo "==> Starting containers with profile: $PROFILE"
-docker compose -f docker-compose.yml -f docker-compose.$PROFILE.yml up --build -d --force-create
+docker compose -f docker-compose.yml -f docker-compose.$PROFILE.yml up --build -d --force-recreate
 
 echo "==> Done! Containers are running."
