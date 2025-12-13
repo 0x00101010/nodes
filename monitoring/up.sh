64da188c5b4f180d034e70c7ebcaa124d2647b04
Francis Li <francis.li@uniswap.org>
2025-12-13 10:27:27 -0800

    Added host monitoring services

---
+++ monitoring/up.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+echo "==> Starting Monitoring Stack (Prometheus, Node Exporter, cAdvisor, Grafana)"
+
+# Creating shared network if necessary
+NETWORK_NAME="shared_network"
+if ! docker network ls | grep -q $NETWORK_NAME; then
+    echo "==> Creating network: $NETWORK_NAME"
+    docker network create $NETWORK_NAME
+else
+    echo "==> Network $NETWORK_NAME already exists"
+fi
+
+# Start all services
+echo "==> Starting monitoring services"
+docker compose up --build -d
+
+echo ""
+echo "==> Done! Services are running:"
+echo "  - Grafana:       http://localhost:3000"
+echo "  - Prometheus:    http://localhost:9090"
+echo "  - Node Exporter: http://localhost:9100"
+echo "  - cAdvisor:      http://localhost:8080"
